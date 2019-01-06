31003eee969d23f8d1f733997fd5b41c4e251bbe
takamitsu-iida <takamitsu.iida@gmail.com>
2019-01-06 15:16:58 +0900

    [ci skip]

--- static/site/js/iida.topology.3.js
+++ static/site/js/iida.topology.3.js
@@ -10,12 +10,12 @@
       container: document.getElementById('cy'),
 
       layout: {
-        name: 'preset',  // 位置をマニュアル指定
-        // name: 'klay',  // 横向きのツリー状に配置
+        // name: 'preset',  // 位置をマニュアル指定
         // name: 'grid',  // グリッド状にノードを配置
         // name: 'circle',  // 円状にノードを配置
         // name: 'breadthfirst',  // 上から下向きにツリー状に配置
-        padding: 10
+        name: 'klay',  // 横向きのツリー状に配置
+        animate: true
       },
 
       boxSelectionEnabled: true,
@@ -29,8 +29,8 @@
           style: {
             'label': 'data(id)',
             'shape': 'rectangle',  // 'data(type)',
-            'height': 40,
-            'width': 40,
+            'height': 30,
+            'width': 30,
             'background-color': '#fff',
             'background-fit': 'contain',  // 'cover',
             'border-color': '#fff',  // '#000',
@@ -42,7 +42,7 @@
           selector: 'edge',
           style: {
             'curve-style': 'bezier',
-            'width': 6,
+            'width': 3,
             'target-arrow-shape': 'triangle',
             'line-color': '#dd4de2',
             'target-arrow-color': '#dd4de2',
@@ -65,63 +65,37 @@
 
       elements: {
         nodes: [
+          { data: { id: 'a' }, classes: 'router'},
+
+          { data: { id: 'b' }, classes: 'router'},
+          { data: { id: 'c' }, classes: 'router'},
+
+          { data: { id: 'd' }, classes: 'router'},
+
           { data: { id: 'e' }, classes: 'router'},
+
           { data: { id: 'f' }, classes: 'router'},
           { data: { id: 'g' }, classes: 'router'},
+
           { data: { id: 'h' }, classes: 'router'},
         ],
         edges: [
+          { data: { source: 'a', target: 'b' } },
+
+          { data: { source: 'b', target: 'c' } },
+          { data: { source: 'b', target: 'd' } },
+
+          { data: { source: 'c', target: 'e' } },
+          { data: { source: 'd', target: 'e' } },
+
           { data: { source: 'e', target: 'f' } },
-          { data: { source: 'f', target: 'g' } },
+          { data: { source: 'e', target: 'g' } },
+
+          { data: { source: 'f', target: 'h' } },
           { data: { source: 'g', target: 'h' } },
         ]
       }
 
-    });
-
-    var allNodes = cy.nodes();
-
-    // 横一直線に並べる
-    allNodes.makeLayout({
-      name: 'grid',
-      boundingBox: { x1: 100, x2: 600, y1: 100, y2: 400 },
-      cols: allNodes.length + 2,
-      fit: false
-    }).run();
-
-    cy.automove({
-      nodesMatching: cy.$('#e, #f, #g, #h'),
-      reposition: function( node ) {
-        // ドラッグしているノードならその位置を返却
-        if (node.grabbed()) {
-          return node.position();
-        }
-
-        // それ以外のノードに対しては、Y座標はドラッグしているノードと同じものを返却する
-        var pos = {
-          x: node.position('x'),
-          y: node.position('y')
-        };
-
-        allNodes.forEach(function(n) {
-          if (n.grabbed()) {
-            pos.y = n.position('y');
-          }
-        });
-
-        return pos;
-      },
-      when: 'matching'
-    });
-
-    cy.fit( 100 ); // fit to all the layouts
-
-    // .automove-viewport nodes kept in viewport (even if added after this call)
-    // convenient but less performant than `nodesMatching: collection`
-
-    cy.automove({
-      nodesMatching: '.automove-viewport',
-      reposition: 'viewport'
     });
 
   };
